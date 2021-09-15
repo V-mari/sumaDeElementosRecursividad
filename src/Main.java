dd4460ea5c8a8c4c35a55fd77d10d11c7b35fc8c
V-mari <violettrujillo5@gmail.com>
2021-09-15 08:54:06 -0400

    suma de elementos de un vector

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args, Object Recursividad) {
+
+        // System.out.println(Ejercicios.mcd(412,184));
+        int[] vector = {4, 4, 6};
+        System.out.println(Ejercicios.sumaVector(vector, 0));
+    }
+
+}
